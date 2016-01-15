b561d0ca7155e459a53267a069e995b5aa6bb338
Matt Vasquez <mattvasquez@gmail.com>
2016-01-15 00:23:03 -0600

    Refactors handling the redirect. Adds production URL

--- prescreen.rb
+++ prescreen.rb
@@ -7,7 +7,7 @@ module HPS
     attr_accessor :data, :url, :headers
 
     def initialize
-      self.url = "http://hps-dev-prescreen.azurewebsites.net/api/v1/applicant_sandbox"
+      self.url = "http://hps-dev-prescreen.azurewebsites.net/api/v1/applicants"
       self.data = {
         fullName: 'Matt Vasquez',
         email: 'mattvasquez@gmail.com',
@@ -28,23 +28,24 @@ module HPS
       response = http.request(request)
 
       if response.kind_of?(Net::HTTPRedirection)
-        puts "Getting applicant info..."
-
-        loc = response['location']
-
-        puts loc
-
-        red_uri = URI.parse(loc)
-        red_http = Net::HTTP.new(red_uri.host, red_uri.port)
-        red_request = Net::HTTP::Get.new(red_uri.request_uri, headers)
-        red_response = red_http.request(red_request)
-
-        puts "STATUS: #{red_response.code}"
-        puts JSON.pretty_generate(JSON.parse(red_response.body))
+        handle_redirect(response['location'])
       else
         puts "STATUS: #{response.code}"
         puts JSON.pretty_generate(JSON.parse(response.body))
       end
+    end
+
+    def handle_redirect(loc)
+      puts "Getting applicant info..."
+      puts loc
+
+      red_uri = URI.parse(loc)
+      red_http = Net::HTTP.new(red_uri.host, red_uri.port)
+      red_request = Net::HTTP::Get.new(red_uri.request_uri, headers)
+      red_response = red_http.request(red_request)
+
+      puts "STATUS: #{red_response.code}"
+      puts JSON.pretty_generate(JSON.parse(red_response.body))
     end
   end
 end
